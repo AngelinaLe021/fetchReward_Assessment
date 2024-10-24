415c4991515c63339e25ba6186494d0475b40873
AngelinaLe021 <81125931+AngelinaLe021@users.noreply.github.com>
2024-10-24 13:48:01 -0700

    post1

--- receipt/cmd/main.go
+++ receipt/cmd/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/AngelinaLe021/fetchReward_Assessment/receipt/models"
 )
 
+
 func main() {
-	database.ConnectDb()
+	// database.ConnectDb()
 	app := fiber.New()
 
 	setupRoutes(app)
--- receipt/cmd/routes.go
+++ receipt/cmd/routes.go
@@ -3,8 +3,12 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/AngelinaLe021/fetchReward_Assessment/receipt/handlers"
+	// "github.com/AngelinaLe021/fetchReward_Assessment/receipt/models"
 )
 
 func setupRoutes(app *fiber.App) {
 	app.Get("/", handlers.Home)
+
+	app.Post("/receipts/process", handlers.ProcessReceipt)
+	app.Get("/receipts/:id/points", handlers.Point)
 }
